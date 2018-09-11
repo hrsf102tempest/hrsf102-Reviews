6af69c94ce879324c285ce8b1127ea1fb7f96eff
Richard Hahn <rhahn07@yahoo.com>
2018-09-11 19:08:26 -0400

    Generated data and started setup

---
+++ data/generateReviewData.js
@@ -0,0 +1,52 @@
+var faker = require('faker');var restaurantData = require('./fecData.js')
+var restaurantData = require('./fecData.js')
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "yelpReviews"
+});
+
+var review = function(restaurantId) {
+  var obj = {
+	restaurantId: restaurantId,
+	reviewerId: Math.ceil(Math.random() * Math.floor(25)),
+  stars: Math.floor(Math.random() * Math.floor(5)),
+  reviewDate: faker.date.recent(),
+  checkIns: Math.floor(Math.random() * Math.floor(5)),
+  textBody: faker.lorem.paragraphs(Math.ceil(Math.random() * Math.floor(4)))
+  };
+  return obj;
+}
+
+var reviews = function() {
+  var reviews = [];
+  for (var i = 1; i <= data.length; i++){
+	var numberOfReviews = Math.ceil(Math.random() * Math.floor(10));
+	    for (var j = 0; j < numberOfReviews; j++) {
+	    	reviews.push(review(i));
+	    }
+  }
+  return reviews;
+}
+  
+var dataArray = reviews();  
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  
+  for (var i = 0; i < dataArray.length; i++){
+    var sql = `INSERT INTO reviews (restaurantId, reviewerId, stars, reviewDate, checkIns, textBody) VALUES ("${dataArray[i].restaurantId}","${dataArray[i].reviewerId}","${dataArray[i].stars}","${dataArray[i].reviewDate}","${dataArray[i].checkIns}", "${dataArray[i].textBody}")`;
+  	con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+    });
+  }  
+});
+
+con.end(function(err) {
+  console.log('The connection is terminated now');
+});
+
